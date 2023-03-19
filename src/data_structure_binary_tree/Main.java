f8d64f48c032836236f5874541c578dcb6ec5958
Jahadul-Rakib <rakibdiu2015@gmail.com>
2023-03-19 11:55:35 +0600

    add

---
+++ src/data_structure_binary_tree/Main.java
@@ -0,0 +1,7 @@
+package data_structure_binary_tree;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/data_structure_tree/Main.java
@@ -0,0 +1,7 @@
+package data_structure_tree;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
